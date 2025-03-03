9c01db27a9eb5925a5ee18ea7af7949f1a2ea040
nickofca <nickfzca@gmail.com>
2025-03-03 12:36:30 -0800

    Disable ACL

--- tf/plugin.tf
+++ tf/plugin.tf
@@ -1,9 +1,8 @@
 resource "aws_s3_bucket" "plugin_bucket" {
   bucket = var.plugin_bucket_name
-  acl    = "public-read"
 
   website {
-    index_document = "index.html"   # You can use a dummy index.html if desired
+    index_document = "index.html"   # You can supply a dummy index.html if desired
     error_document = "error.html"
   }
 }
@@ -13,7 +12,7 @@ resource "aws_s3_bucket_object" "openapi" {
   key          = "openapi.yaml"
   source       = "${path.module}/../openapi.yaml"
   content_type = "text/yaml"
-  acl          = "public-read"
+  # acl removed
 }
 
 resource "aws_s3_bucket_object" "plugin_manifest" {
@@ -21,5 +20,31 @@ resource "aws_s3_bucket_object" "plugin_manifest" {
   key          = "ai-plugin.json"
   source       = "${path.module}/../ai-plugin.json"
   content_type = "application/json"
-  acl          = "public-read"
+  # acl removed
+}
+
+resource "aws_s3_bucket_policy" "plugin_bucket_policy" {
+  bucket = aws_s3_bucket.plugin_bucket.bucket
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.plugin_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "plugin_bucket_block" {
+  bucket = aws_s3_bucket.plugin_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
