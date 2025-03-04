17340496c652e33a412ca3679f65f55caf662f7d
nickofca <nickfzca@gmail.com>
2025-03-03 21:11:54 -0800

    Adjust output format

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,6 +1,6 @@
 output "api_endpoint" {
   description = "The URL of the API Gateway endpoint"
-  value       = "${aws_api_gateway_deployment.diet_tracker_deployment.invoke_url}/prod/"
+  value       = "${aws_api_gateway_deployment.diet_tracker_deployment.invoke_url}"
 }
 
 output "openapi_url" {
