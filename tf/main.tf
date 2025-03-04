a4a82248eb03f3f8460c8825644e061021197c84
nickofca <nickfzca@gmail.com>
2025-03-03 20:14:00 -0800

    Upgrade to python 3.12

--- tf/main.tf
+++ tf/main.tf
@@ -106,7 +106,7 @@ resource "aws_lambda_function" "diet_tracker_lambda" {
   function_name    = "${var.namespace}-diet_tracker_function"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.8"
+  runtime          = "python3.12"
   source_code_hash = filebase64sha256("lambda.zip")
   timeout          = 10
 
